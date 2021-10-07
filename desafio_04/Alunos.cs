e71756389680b923f7fbf5148815df1065eb3219
pabloluizde <pablo2006_luiz@hotmail.com>
2021-10-07 19:32:37 -0300

    Desafio 04 concluido

---
+++ desafio_04/Alunos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace desafio_04
+{
+    public class Alunos
+    {
+        public string Nome { get; set; }
+
+        public Alunos()
+        { }
+
+        public Alunos(string nome)
+        {
+               Nome = nome;
+           
+        }
+    }
+}
---
+++ desafio_04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace desafio_04
+{
+    class AlunosList
+    {
+        static void Main(string[] args)
+        {        
+            
+            List<string> aluno = new List<string>();
+
+            aluno.Add("Maria");
+            aluno.Add("Claudio");
+            aluno.Add("Paulo");
+            aluno.Add("Pedro");
+
+            foreach(string obj in aluno)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
